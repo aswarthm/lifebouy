190c357d42add607f86dc3b68ebdbaaec20585ca
aswarth <m.aswarthreddy@gmail.com>
2023-03-25 21:22:15 +0530

    css

--- node/page.h
+++ node/page.h
@@ -1,6 +1,5 @@
 const char MAIN_page[] = R"=====(
 
-
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="UTF-8">
@@ -8,23 +7,19 @@ const char MAIN_page[] = R"=====(
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <head>
-  <link rel="stylesheet" href="../styles.css">
-  <title>LIFE BOUY</title>
-</head> 
+  <link rel="stylesheet" href="./styles.css">
+  <title>LIFEBOUY</title>
+</head>
 
-<form action="/get">
+<body>
+
+<div class="title">
+  <h1>LIFEBOUY</h1>
+</div>
+<form action="">
   <div>
-    <h1>LIFE BOUY</h1>
-  </div>
-  <div>
-    <div>
+    <div class="help">
       <h2>HELP PRIORITY</h2>
-    </div>
-
-
-
-    <div>
-
       <input type="checkbox" id="medical" name="medical">
       <label for="medical"> Medical Emergency</label>
       <br>
@@ -34,53 +29,99 @@ const char MAIN_page[] = R"=====(
       <input type="checkbox" id="otherReq" name="food">
       <label for="otherReq"> Other Requirements</label>
       <br>
+      <br>
     </div>
+    <div class="info">
+      <h2>More Information</h2>
 
+      <label for="quantity">Number of people with you:</label>
+      <input type="number" id="quantity" name="quantity" min="0" max="1000">
 
-  </div>
-  <div>
-    <h2>More Information</h2>
-
-    <label for="quantity">Number of people with you:</label>
-    <input type="number" id="quantity" name="quantity" min="0" max="1000">
-    <h4 class="gnrltext"> Do the people include...</h4>
-
-    <input type="checkbox" id="women" name="women">
-    <label for="women"> Women</label>
+      <input type="checkbox" id="women" name="women">
+      <label for="women"> Women</label>
+      <br>
+      <input type="checkbox" id="children" name="children">
+      <label for="children"> Children</label>
+      <br>
+      <input type="checkbox" id="disabled" name="disabled">
+      <label for="disabled"> Disabled</label>
+      <br>
+      <input type="checkbox" id="injured" name="injured">
+      <label for="injured"> Injured</label>
+      <br>
+      <br>
+      <div>
+        <label for="chall">Any other challenges</label>
+        <br>
+        <input class="textbox" type="text" id="chall" name="chall">
+        <br>
+      </div>
+    </div>
     <br>
-    <input type="checkbox" id="children" name="children">
-    <label for="children"> Children</label>
-    <br>
-    <input type="checkbox" id="disabled" name="disabled">
-    <label for="disabled"> Disabled</label>
-    <br>
-    <input type="checkbox" id="injured" name="injured">
-    <label for="injured"> Injured</label>
-    <br>
-
-  </div>
-  <br>
-  <div>
-    <label for="chall">Any other challenges</label>
-    <br>
-    <input type="text" id="chall" name="chall">
-    <br>
-  </div>
-  <input type="hidden" id="loc" name="loc" value="0,0">
-  <input type="submit" value="Submit">
+    <input class="btn" type="submit" value="Submit">
+    
+    <div class="SOS">
+      <h4> Stay Calm!! If any undesirable challenges, press the SOS button</h4>
+      <a href="">
+        <button class="btn">SOS</button>
+      </a>
+    </div>
 </form>
-
-<div class="SOS">
-  <h4> Stay Calm!! If any undesirable challenges, press the SOS button</h4>
-  <a href="">
-    <button>Visit Google</button>
-  </a>
-</div>
-</section>
-<div id="ok"></div>
 </body>
 
 </html>
+
+<style>
+*{
+  padding: 5px;
+  margin: 5px;
+}
+
+body{
+  background-color: #FFF5E4;
+  font-family: 'Roboto', sans-serif;
+}
+
+
+.info {
+  border : 5px solid;
+  border-radius: 10px;
+  border-color: #850000;
+  margin-bottom: 10px;
+}
+
+.help{
+  border-radius: 10px;
+  border:5px solid;
+  border-color: #850000;
+  margin-bottom: 10px;
+}
+
+.title{
+  margin-top: -20px;
+  margin-left: -20px;
+  margin-right: -20px;
+  text-align: center;
+  color: whitesmoke;
+  background-color: #850000;
+  border-bottom: solid 4px rgb(48, 48, 48);
+  font-size: xx-large;
+}
+
+.textbox{
+  padding: 8px;
+  border-radius: 4px;
+  box-shadow:1px 2px black;
+}
+
+.btn{
+  box-shadow:1px 2px black;
+  padding: 20px 10px;
+  border-radius: 12px;
+  width: 100%;
+  font-size: xx-large;
+}
+</style>
 
 
 )=====";
